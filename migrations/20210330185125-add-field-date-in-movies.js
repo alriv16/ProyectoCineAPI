f7773bee681a7249dadc50fd19278d4100e80afc
Alejandra <alriv16@gmail.com>
2021-03-30 12:53:14 -0600

    creating field date in movie

--- migrations/20210330185125-add-field-date-in-movies.js
+++ migrations/20210330185125-add-field-date-in-movies.js
@@ -2,20 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('movies', 'date', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.removeColumn('movies', 'date');
+  },
 };
